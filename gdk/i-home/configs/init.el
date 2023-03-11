6136cc4f2ef328463ad8fef5146b3709ca56b7e0
Dilip <igoldlip@gmail.com>
2023-03-11 19:06:29 +0530

    Added Modus theme customization to emacs. Best Theme Ever!

--- gdk/i-home/configs/init.el
+++ gdk/i-home/configs/init.el
@@ -941,7 +941,26 @@ org-modern-list
   :delight " ⊛")
                                         ; "Ⓐ" "⊗"
 
-(setq modus-themes-org-blocks 'gray-background)
+(setq modus-themes-italic-constructs t
+      modus-themes-bold-constructs t
+      modus-themes-mixed-fonts t
+      modus-themes-variable-pitch-ui t
+      modus-themes-custom-auto-reload t
+      modus-themes-disable-other-themes t
+      modus-themes-prompts '(italic bold)
+      modus-themes-org-blocks 'gray-background
+      modus-themes-completions
+      '((matches . (extrabold))
+        (selection . (semibold italic text-also)))
+
+      modus-themes-org-blocks 'gray-background
+
+      modus-themes-headings
+      '((1 . (variable-pitch 1.5))
+        (2 . (1.3))
+        (agenda-date . (1.3))
+        (agenda-structure . (variable-pitch light 1.8))
+        (t . (1.1))))
 (load-theme 'modus-vivendi t)
 
 (use-package beframe)

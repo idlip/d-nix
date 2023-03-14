53c5441600c4692c345f650383f994d2f096a507
Dilip <igoldlip@gmail.com>
2023-03-14 18:33:41 +0530

    added fixme tag to flake

--- flake.nix
+++ flake.nix
@@ -2,20 +2,32 @@
   description = "D Nixyfied ❄️ Flaky Station";
 
   inputs = {
+
+    # Change it to table, if you want stable channel
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    # Idk where is this used, I just have it
     nixpkgs-wayland.url = "github:nix-community/nixpkgs-wayland";
+
+    # For firefox extension
     nur.url = "github:nix-community/nur";
-    hosts.url = github:StevenBlack/hosts;
+
+    # For Adblocking and making internet usable
+    hosts.url = "github:StevenBlack/hosts";
+
+    # Oh yeah, the best wayland compositor out there
     hyprland = {  
       url = "github:hyprwm/Hyprland";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     xdg-portal-hyprland.url = "github:hyprwm/xdg-desktop-portal-hyprland";
 
+    # Greatest editor of all time. (if you shill on vim, give emacs a try and see first, it objective to individuals)
     emacs-overlay = {   
       url = "github:nix-community/emacs-overlay";
     };
+
+    # Home to manage all user configs
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -25,19 +37,22 @@
 
   outputs = inputs @ {self, hyprland, nur, hosts, home-manager, nixpkgs, ...} :
     let
+      # You might check on darwin, or its this is enough
       system = "x86_64-linux";
       pkgs = import nixpkgs {
-  inherit system;
-  config.allowUnfree = true;
+        inherit system;
+        config.allowUnfree = true;
       };
       lib = nixpkgs.lib;
       
   in {
     
     nixosConfigurations = {
+      # #FIXME change 'gdk' to your HOSTNAME
       gdk = lib.nixosSystem {
         inherit system;
         modules = [
+          # Again #FIXME to your HOSTNAME
           ./gdk/configuration.nix
           home-manager.nixosModules.home-manager {
             home-manager.useGlobalPkgs = true;
@@ -46,6 +61,8 @@
               inherit inputs;
               inherit self;
             };
+            # FIXME 'users.i' to your users.USERNAME
+            # And '/gdk/i-home' to your choice of path (eg: HOSTNAME/USERNAME) It is just path where nix files are tangled, so you have to enter this into d-setup.org file too
             home-manager.users.i = ./gdk/i-home ;
           }
           hosts.nixosModule

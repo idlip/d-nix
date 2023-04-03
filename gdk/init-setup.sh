3541e39a5cc415b966be405375d968728034c6b4
Dilip <igoldlip@gmail.com>
2023-04-03 22:36:28 +0530

    Small edit

--- gdk/init-setup.sh
+++ gdk/init-setup.sh
@@ -1,8 +1,12 @@
-#! /usr/bin/env bash
+#!/usr/bin/env bash
+# Read and edit these once accordingly
 
 echo "Installing System Flake"
 sudo cp -f /etc/nixos/hardware-configuration.nix /home/i/.DLIP/SETUP/gdk/hardware-configuration.nix
-cd ~/.DLIP/SETUP
+
+# Edit it your file path
+
+#cd /home/i/.DLIP/SETUP
 sudo nixos-rebuild switch --flake .#gdk
 
 echo "Copying Installation Checklist"

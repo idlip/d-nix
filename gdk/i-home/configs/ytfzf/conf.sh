63c1c02ffc9dfd35a5568d03e5049507ce0036c4
Dilip <igoldlip@gmail.com>
2023-04-24 23:26:24 +0530

    Change of fuzzel to rofi

--- gdk/i-home/configs/ytfzf/conf.sh
+++ gdk/i-home/configs/ytfzf/conf.sh
@@ -8,8 +8,8 @@ show_thumbnails=0
 external_menu () {
     #use rofi instead of dmenu
 #    rofi -dmenu -i -theme-str '@import "menu.rasi" window {height:60%;width: 70%;}' -p "$1"
-#    rofi -dmenu -i -config ~/.config/rofi/list.rasi -p "  Play "  
-     fuzzel -d -p " 磊 Play " -w 90 -l 22
+  rofi -dmenu -i -config ~/.config/rofi/list.rasi -p "  Play "  
+     # fuzzel -d -p " 磊 Play " -w 90 -l 22
     # wofi -dib -W 90% -H 80% 
      # bemenu -W 0.98 -l 22 -p '  Play '
 #    dmenu -i -l 20

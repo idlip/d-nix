d5e418cb445e08de1628c86d83bd439b4c7f61e8
Dilip <igoldlip@gmail.com>
2023-04-28 22:25:38 +0530

    Comment hyprland

--- gdk/i-home/default.nix
+++ gdk/i-home/default.nix
@@ -6,7 +6,7 @@
   config.home.extraOutputsToInstall = ["doc" "devdoc"];
   imports = [
     ./homeman.nix
-    inputs.hyprland.homeManagerModules.default
+    # inputs.hyprland.homeManagerModules.default
     # inputs.nur.nixosModules.nur
   ];
 }

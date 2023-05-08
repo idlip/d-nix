eb03569a3389d28470ed4bd338bea1fd2ebed152
Dilip <igoldlip@gmail.com>
2023-05-08 18:47:30 +0530

    Chnage of version

--- gdk/i-home/default.nix
+++ gdk/i-home/default.nix
@@ -1,7 +1,7 @@
 {
   inputs, pkgs, config, lib, self, ...}:
 {
-  config.home.stateVersion = "22.11";
+  config.home.stateVersion = "23.05";
   config.home.extraOutputsToInstall = ["doc" "devdoc"];
   imports = [
     ./homeman.nix

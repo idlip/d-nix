9ce930a70efac6a1a725fe3b63c43f7532498529
Dilip <igoldlip@gmail.com>
2023-05-18 19:14:43 +0530

    Bemenu is quick!

--- gdk/i-home/configs/ytfzf/conf.sh
+++ gdk/i-home/configs/ytfzf/conf.sh
@@ -6,12 +6,11 @@ show_thumbnails=0
 ##}}}
 
 external_menu () {
-    #use rofi instead of dmenu
 #    rofi -dmenu -i -theme-str '@import "menu.rasi" window {height:60%;width: 70%;}' -p "$1"
-  rofi -dmenu -i -config ~/.config/rofi/list.rasi -p "  Play "  
+     #  rofi -dmenu -i -config ~/.config/rofi/list.rasi -p "  Play "  
      # fuzzel -d -p " 磊 Play " -w 90 -l 22
     # wofi -dib -W 90% -H 80% 
-     # bemenu -W 0.98 -l 22 -p '  Play '
+     bemenu -W 0.98 -l 24 -p '  Play '
 #    dmenu -i -l 20
 }
 

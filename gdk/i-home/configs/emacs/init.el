f001b729fc02634170b91caef50446ead83ce315
Dilip <igoldlip@gmail.com>
2023-05-19 20:39:50 +0530

    Ement org-message filter works

--- gdk/i-home/configs/emacs/init.el
+++ gdk/i-home/configs/emacs/init.el
@@ -1889,16 +1889,29 @@ selected color."
 ;; (advice-add 'eww-readable :after #'d/bionic-read)
 
 (use-package ement
-  :defer t
-  :hook (ement-room-compose . ement-room-compose-org)
   :bind (:map ement-room-minibuffer-map
               ("<f6>" . ement-room-compose-from-minibuffer))
   (:map ement-room-mode-map
         ("M-<" . ement-room-scroll-down-command))
+  :custom
+  (ement-room-send-message-filter 'ement-room-send-org-filter)
+  (ement-room-message-format-spec "%S> %L%B%r%R%t")
+  (ement-room-list-avatars nil)
+  (ement-save-sessions t)
   :config
-  (setq ement-room-send-message-filter 'ement-room-send-org-filter)
-  (setq ement-room-message-format-spec "%S> %L%B%r%R%t")
-  (setq ement-room-list-avatars nil))
+  ;; copied from viru (ement github)
+  (defun d/ement-connect ()
+    (interactive)
+    (if (ement--read-sessions)
+        (call-interactively #'ement-connect)
+      (let* ((found (auth-source-search :max 1
+                                        :host "matrix.org"
+                                        :port "8448"
+                                        :require '(:user :secret)))
+             (entry (nth 0 found))
+             (password (funcall (plist-get entry :secret)))
+             (user (plist-get entry :user)))
+        (ement-connect :user-id user :password password)))))
 
 (use-package ox-hugo
   :after ox)

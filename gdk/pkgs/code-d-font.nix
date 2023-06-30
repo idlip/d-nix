b69787a0349257709a87ca302d625981c76d6c3d
Dilip <igoldlip@gmail.com>
2023-06-30 13:52:23 +0530

    Custom package

---
+++ gdk/pkgs/code-d-font.nix
@@ -0,0 +1,27 @@
+{ lib, fetchgit, stdenvNoCC }:
+
+stdenvNoCC.mkDerivation rec {
+  pname = "code-d-font";
+  version = "1.085";
+
+  src = fetchgit {
+    url = "https://github.com/idlip/code-d-font";
+    sparseCheckout = [
+      "fonts"
+    ];
+    sha256 = "sha256-u4ZzBk00X/IreGb5kLMe3lQIBawfRq+Ss0oZaVAZpt4=";
+  };
+
+  installPhase = ''
+    install -D -t $out/share/fonts/truetype/ $(find $src -type f -name '*.ttf')
+  '';
+
+
+  meta = with lib; {
+    homepage = "https://github.com/arrowtype/recursive-code-config/";
+    description = "A variable font family for code & UI";
+    license = licenses.ofl;
+    maintainers = [ maintainers.idlip ];
+    platforms = platforms.all;
+  };
+}

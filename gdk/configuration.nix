e2d50bfcd8e3e236476ea7f4a1e2bc4c5208a854
Dilip <igoldlip@gmail.com>
2023-07-27 22:08:19 +0530

    some addition to pipewire

--- gdk/configuration.nix
+++ gdk/configuration.nix
@@ -301,9 +301,11 @@ services = {
     enable = true;
     alsa = {
       enable = true;
+      support32Bit = true;
     };
     wireplumber.enable = true;
     pulse.enable = true;
+    jack.enable = true;
   };
 };
 

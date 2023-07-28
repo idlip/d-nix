b24a92933995b01cba48c059e618948e7863fda0
Dilip <igoldlip@gmail.com>
2023-07-28 22:27:34 +0530

    REVAMP begin...!

--- gdk/i-home/default.nix
+++
@@ -1,9 +0,0 @@
-{ inputs, pkgs, config, lib, self, ...}:
-{
-  config.home.stateVersion = "23.05";
-  config.home.extraOutputsToInstall = ["doc" "devdoc"];
-  imports = [
-    ./homeman.nix
-    # inputs.nur.nixosModules.nur
-  ];
-}

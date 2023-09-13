3c9333ddd4fb94b74f922fe3f6631d39abc3af33
Dilip <igoldlip@gmail.com>
2023-09-13 20:03:08 +0530

    emacs: added languagetool support

--- home/programs/emacs.nix
+++ home/programs/emacs.nix
@@ -6,22 +6,25 @@
     package = pkgs.emacs29-pgtk;
     extraPackages = (epkgs: (with epkgs; [
       treesit-grammars.with-all-grammars
-      eat vundo undo-fu-session flycheck helpful ox-pandoc
+      eat vundo undo-fu-session flycheck helpful flycheck-languagetool ox-pandoc
       no-littering rainbow-delimiters rainbow-mode
       vertico orderless consult marginalia embark org-modern corfu cape corfu-terminal
       org olivetti nerd-icons async dirvish
       embark-consult consult-eglot consult-flycheck markdown-mode nix-mode
       reddigg hnreader mingus which-key magit webpaste org-present
-      # pdf-tools nov (using built-in doc-view)
       shrface shr-tag-pre-highlight nov devdocs-browser
       beframe denote tempel tempel-collection
       sdcv elfeed elfeed-org link-hint powerthesaurus jinx meow
       doom-modeline hide-mode-line el-fetch ox-hugo
       ement kind-icon speed-type vc-backup aria2
-      ess elpy
+      ess
     ])
     );
   };
+
+  home.packages = with pkgs; [
+    languagetool
+  ];
 
   home.file.".config/emacs" = {
     recursive = true;

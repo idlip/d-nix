529f5bcde237461766716a25e2053557f97f6c5a
Dilip <igoldlip@gmail.com>
2023-10-20 23:06:04 +0530

    simple starship

--- home/shell/starship.nix
+++ home/shell/starship.nix
@@ -8,30 +8,8 @@
     settings = {
       add_newline = true;
       scan_timeout = 5;
-      character = {
-	      error_symbol = " [](bold red)";
-	      success_symbol = "[](bold green)";
-	      vicmd_symbol = "[](bold yellow)";
-	      # format = "$directory$all$character](bold)[  ](bold blue)";
-      };
 
-      git_status = {
-        deleted = "✗";
-        modified = "✶";
-        staged = "✓";
-        stashed = "≡";
-      };
-
-      git_commit = {commit_hash_length = 4;};
       line_break.disabled = false;
-      lua.symbol = "[󰢱](blue) ";
-      python.symbol = "[](blue) ";
-      directory.read_only = " ";
-
-      nix_shell = {
-        symbol = " ";
-        heuristic = false;
-      };
 
       hostname = {
 	      ssh_only = true;

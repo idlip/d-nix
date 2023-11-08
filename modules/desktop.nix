177857f9b5f3130d2ceccf7bd177ea587324c1aa
Dilip <igoldlip@gmail.com>
2023-11-08 19:25:53 +0530

    nix: add more tlp config

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -7,6 +7,7 @@
 
   programs = {
     dconf.enable = true;
+    noisetorch.enable = true; # virtual noise suppressor
   };
 
   powerManagement = {
@@ -55,14 +56,23 @@
     tlp = {
       enable = true;
       settings = {
-        CPU_SCALING_GOVERNOR_ON_AC = "powersave";
+        CPU_SCALING_GOVERNOR_ON_AC = "balance_performance";
         CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
         CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
         CPU_ENERGY_PERF_POLICY_ON_AC = "balance_performance";
 
+        USB_AUTOSUSPEND = 1;
+        DEVICES_TO_DISABLE_ON_STARTUP = "bluetooth";
+        DEVICES_TO_DISABLE_ON_BAT_NOT_IN_USE = "bluetooth";
+
+        CPU_BOOST_ON_AC = 0;
+        CPU_BOOST_ON_BAT = 0;
+        CPU_HWP_DYN_BOOST_ON_AC = 1;
+        CPU_HWP_DYN_BOOST_ON_BAT = 0;
+
         CPU_MIN_PERF_ON_AC = 0;
-        CPU_MAX_PERF_ON_AC = 100;
+        CPU_MAX_PERF_ON_AC = 70;
         CPU_MIN_PERF_ON_BAT = 0;
         CPU_MAX_PERF_ON_BAT = 30;
       };

07cdb7090c58c16e39f3d911b5f4f70d519ed1de
Dilip <igoldlip@gmail.com>
2023-12-14 16:11:20 +0530

    local variable to tangle file on save

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,1 +1,1 @@
-((org-mode . ((eval . (add-hook 'after-save-hook (lambda () (let ((inhibit-redisplay t) (inhibit-message t) (emacs-lisp-mode-hook '())) (org-babel-tangle))) nil t)))))
+(("d-setup.org" . ((org-mode . ((eval . (add-hook 'after-save-hook (lambda () (let ((inhibit-redisplay t) (inhibit-message t) (emacs-lisp-mode-hook '())) (org-babel-tangle))) nil t)))))))

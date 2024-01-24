c9b3ebecf06e9f3358768395a0ce96dc0b4f45e2
Dilip <igoldlip@gmail.com>
2024-01-24 21:34:29 +0530

    local: use alejandra format

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,4 @@
-(("d-setup.org" . ((org-mode . ((eval . (add-hook 'after-save-hook (lambda () (let ((inhibit-redisplay t) (inhibit-message t) (emacs-lisp-mode-hook '())) (org-babel-tangle))) nil t)))))))
+;; (("d-setup.org" . ((org-mode . ((eval . (add-hook 'after-save-hook (lambda () (let ((inhibit-redisplay t) (inhibit-message t) (emacs-lisp-mode-hook '())) (org-babel-tangle))) nil t)))))))
 
 ((nix-mode
-  (mode . nix-format-on-save)))
+  (mode . alejandra-format-on-save)))
